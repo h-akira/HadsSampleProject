99938b44efdb8e7a3b61d09c4e558ae3a2ffb4ae
h-akira <79441679+h-akira@users.noreply.github.com>
2025-03-15 22:30:00 +0900

    fix

--- Lambda/example/views.py
+++ Lambda/example/views.py
@@ -1,6 +1,5 @@
 from hads.shourtcuts import render
-from hads.shourtcuts import login_required
 
-@login_required
 def index(master):
+  context = {}
   return render(master, 'example/index.html', context)
